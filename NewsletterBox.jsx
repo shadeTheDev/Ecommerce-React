5ec8fd66dd50d497f52f6ed5d4c68b4e59fa0e95
Thomas <shadeTheDev@gmail.com>
2024-10-16 00:43:41 -0500

    ReAdding Finished Files pt.2
    Latest Collection, Navbar, Newsletter, Policy, ProductItem, Related Products, Searchbar, Title component

---
+++ NewsletterBox.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const NewsletterBox = () => {
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <div className="text-center">
+      <p className="text-[30px] font-medium text-gray-800">
+        Subscribe now & get 20% off
+      </p>
+      <p className="text-gray-400 mt-3">
+        Subscribe to Forever Apparel today and get deals and upcoming designs
+        early
+      </p>
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3">
+        <input
+          className="w-full sm:flex-1 outline-none"
+          type="email"
+          placeholder="Enter your email"
+        />
+        <button
+          type="submit"
+          className="bg-black text-white text-xs px-10 py-4">
+          Subscribe
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default NewsletterBox;
